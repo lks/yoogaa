63bb1a19f2f9c3cbcb35e81bb5ab947c4d02e59f
j.calabrese <j.calabrese@meetic-corp.com>
2014-10-17 17:20:55 +0200

    test(param project): add grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,8 @@ module.exports = function(grunt) {
     },
     karma: {
       unit: {
-          configFile: 'karma.conf.js'
+        configFile: 'karma.conf.js',
+        singleRun: true
       }
     },
     auto_install: {
