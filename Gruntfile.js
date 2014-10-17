6705bb5f28f8ae86b757697544588028b03d5852
j.calabrese <j.calabrese@meetic-corp.com>
2014-10-17 17:09:03 +0200

    test(param project): add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+          src: 'src/<%= pkg.name %>.js',
+          dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      all: ['app/**/*.js'],
+      options: {
+          jshintrc:true
+      }
+    },
+    karma: {
+      unit: {
+          configFile: 'karma.conf.js'
+      }
+    },
+    auto_install: {
+      local: {},
+      subdir: {
+        options: {
+          cwd: 'subdir',
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
+      }
+    },
+    protractor: {
+      options: {
+        configFile: "protractor.conf.js", // Default config file
+        keepAlive: true, // If false, the grunt process stops when the test fails.
+        noColor: false, // If true, protractor will not use colors in its output.
+        args: {
+          // Arguments passed to the command
+        }
+      },
+      cucumber: {
+        options: {
+          args: {
+            specs: [
+              '**/*.feature'
+            ],
+            framework: 'cucumber',
+            capabilities: {
+              browserName: 'phantomjs'
+            }
+          }
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-auto-install');
+  grunt.loadNpmTasks('grunt-protractor-runner');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-karma');
+
+  // Default task(s).
+  grunt.registerTask('test', ['karma']);
+
+};
